3e383a83a3d4934b4e20096c24a62c3d3cb90458
Bas <30810@ma-web.nl>
2022-05-20 10:00:03 +0200

    added enemy class

---
+++ GameJam/Game/enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameJam.Game
+{
+    class enemy
+    {
+        public char graphic;
+        public Rectangle sprite;
+        public float frame;
+        internal Rectangle rectangle;
+
+        public Point pointA;
+        public Point pointB;
+    }
+}
