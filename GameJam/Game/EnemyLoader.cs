2af86581ef9ca81b44992f9089bffc9bc84b1258
Bas <30810@ma-web.nl>
2022-05-20 10:51:30 +0200

    commands added

--- GameJam/Game/EnemyLoader.cs
+++ GameJam/Game/EnemyLoader.cs
@@ -8,8 +8,11 @@ namespace GameJam.Game
 {
     class EnemyLoader
     {
-        //Een refrence naar de room
-        //Een enemy die 2 punten heeft, om tussen te lopen
+        //Een refrence naar de room?
+        //Een enemy die 2 punten heeft, om tussen te lopen?
         //misschien een mogelijkheid om meerdere enemy's te hebben
+        //misschien net zoals de room.txt een enemy<getal>.txt hebben voor waar die staat en waar ie naar toe gaat
+
+        
     }
 }
