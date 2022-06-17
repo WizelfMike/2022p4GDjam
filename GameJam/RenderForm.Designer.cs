01a8c6a41dd8e39f71bd6aa8cd1f0c73b998df41
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2022-06-17 09:59:18 +0200

    Added the icon, end screen and changed the room layouts

--- GameJam/RenderForm.Designer.cs
+++ GameJam/RenderForm.Designer.cs
@@ -29,6 +29,7 @@ namespace GameJam
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RenderForm));
             this.SuspendLayout();
             // 
             // RenderForm
@@ -36,8 +37,10 @@ namespace GameJam
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RenderForm";
-            this.Text = "Form1";
+            this.Text = "Sir Tresór\'s Epic Adventure for The Milk";
+            this.Load += new System.EventHandler(this.RenderForm_Load_1);
             this.ResumeLayout(false);
 
         }
